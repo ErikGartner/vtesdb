aad67f428502a9ac0a1d878684061249731a6fd4
Erik <xxxsmoizxxx@gmail.com>
2015-02-10 19:35:31 +0100

    Inventories are now updated when the cards database is reset.

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -10,34 +10,38 @@ Meteor.startup(function () {
 
     var cards = JSON.parse(Assets.getText("vteslib.json"));
     for(var i = 0; i < cards.length; i++){
-      Cards.insert({name: cards[i]["Name"],
-                    text: cards[i]["Card Text"],
-                    requirement: cards[i]["Requirement"],
-                    clan: cards[i]["Clan"],
-                    discipline: cards[i]["Discipline"],
-                    bloodCost: cards[i]["Blood Cost"],
-                    poolCost: cards[i]["Pool Cost"],
-                    set: cards[i]["Set"],
-                    banned: cards[i]["Banned"],
-                    type: cards[i]["Type"],
-                    cardType: 'lib'});
+      var id = Cards.insert({name: cards[i]["Name"],
+                            text: cards[i]["Card Text"],
+                            requirement: cards[i]["Requirement"],
+                            clan: cards[i]["Clan"],
+                            discipline: cards[i]["Discipline"],
+                            bloodCost: cards[i]["Blood Cost"],
+                            poolCost: cards[i]["Pool Cost"],
+                            set: cards[i]["Set"],
+                            banned: cards[i]["Banned"],
+                            type: cards[i]["Type"],
+                            cardType: 'lib'});
+
+      Inventories.update({cardName: cards[i]["Name"]}, {$set: {cardId: id}}, {multi: true});
     }
 
     var cards = JSON.parse(Assets.getText("vtescrypt.json"));
     for(var i = 0; i < cards.length; i++){
-      Cards.insert({name: cards[i]["Name"],
-                    text: cards[i]["Card Text"],
-                    clan: cards[i]["Clan"],
-                    adv: cards[i]["Adv"],
-                    capacity: cards[i]["Capacity"],
-                    group: cards[i]["Group"],
-                    disciplines: cards[i]["Disciplines"],
-                    title: cards[i]["Title"],
-                    artist: cards[i]["Artist"],
-                    set: cards[i]["Set"],
-                    banned: cards[i]["Banned"],
-                    type: cards[i]["Type"],
-                    cardType: 'crypt'});
+      var id = Cards.insert({name: cards[i]["Name"],
+                            text: cards[i]["Card Text"],
+                            clan: cards[i]["Clan"],
+                            adv: cards[i]["Adv"],
+                            capacity: cards[i]["Capacity"],
+                            group: cards[i]["Group"],
+                            disciplines: cards[i]["Disciplines"],
+                            title: cards[i]["Title"],
+                            artist: cards[i]["Artist"],
+                            set: cards[i]["Set"],
+                            banned: cards[i]["Banned"],
+                            type: cards[i]["Type"],
+                            cardType: 'crypt'});
+
+      Inventories.update({cardName: cards[i]["Name"]}, {$set: {cardId: id}}, {multi: true});
     }
 
     Cards.insert({dbmetadata: "version", version: CARD_DATABASE_VERSION});
