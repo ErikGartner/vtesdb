65bd9c036bdc1537a28c254c97618bb3bb686e5e
Erik <xxxsmoizxxx@gmail.com>
2015-02-17 22:12:43 +0100

    Fixed minor bug with reloading cards also removed cards DB version in favor of using commit version

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,58 +2,61 @@ Meteor.startup(function () {
 
   Assets.getText('version.json', function(err, data){
 
-    Version.remove({});
+    var oldVer = Version.findOne({});
     if(err == null) {
-      Version.insert(JSON.parse(data));
+      var ver = JSON.parse(data);
     } else {
-      Version.insert({branch:'unknown branch', commit:'', timestamp:''});
+      var ver = {branch:'', commit:'detached', timestamp:''};
+    }
+
+    if(!oldVer || oldVer.commit != ver.commit){
+
+      console.log('Updating carddatabase and git version..');
+      Version.remove({});
+      Version.insert(ver);
+      Cards.remove({});
+
+
+      console.log('Reading all cards...');
+
+      var cards = JSON.parse(Assets.getText('vteslib.json'));
+      for(var i = 0; i < cards.length; i++){
+        Cards.insert({name: cards[i]['Name'],
+                      text: cards[i]['Card Text'],
+                      requirement: cards[i]['Requirement'],
+                      clan: cards[i]['Clan'],
+                      discipline: cards[i]['Discipline'],
+                      bloodCost: cards[i]['Blood Cost'],
+                      poolCost: cards[i]['Pool Cost'],
+                      set: cards[i]['Set'],
+                      banned: cards[i]['Banned'],
+                      type: cards[i]['Type'],
+                      cardId: cards[i]['id'],
+                      cardType: 'lib'});
+      }
+
+      var cards = JSON.parse(Assets.getText('vtescrypt.json'));
+      for(var i = 0; i < cards.length; i++){
+        Cards.insert({name: cards[i]['Name'],
+                      text: cards[i]['Card Text'],
+                      clan: cards[i]['Clan'],
+                      adv: cards[i]['Adv'],
+                      capacity: cards[i]['Capacity'],
+                      group: cards[i]['Group'],
+                      disciplines: cards[i]['Disciplines'],
+                      title: cards[i]['Title'],
+                      artist: cards[i]['Artist'],
+                      set: cards[i]['Set'],
+                      banned: cards[i]['Banned'],
+                      type: cards[i]['Type'],
+                      cardId: cards[i]['id'],
+                      cardType: 'crypt'});
+      }
+
+      console.log('Done!');
+
     }
   });
 
-  var meta = Cards.findOne({dbmetadata: 'version'});
-  if(!meta || meta.version != CARD_DATABASE_VERSION){
-    console.log('Cards database version incorrect; purging.');
-    Cards.remove({});
-  }
-
-  if (Cards.find().count() === 0) {
-
-    var cards = JSON.parse(Assets.getText('vteslib.json'));
-    for(var i = 0; i < cards.length; i++){
-      Cards.insert({name: cards[i]['Name'],
-                    text: cards[i]['Card Text'],
-                    requirement: cards[i]['Requirement'],
-                    clan: cards[i]['Clan'],
-                    discipline: cards[i]['Discipline'],
-                    bloodCost: cards[i]['Blood Cost'],
-                    poolCost: cards[i]['Pool Cost'],
-                    set: cards[i]['Set'],
-                    banned: cards[i]['Banned'],
-                    type: cards[i]['Type'],
-                    cardId: cards[i]['id'],
-                    cardType: 'lib'});
-    }
-
-    var cards = JSON.parse(Assets.getText('vtescrypt.json'));
-    for(var i = 0; i < cards.length; i++){
-      Cards.insert({name: cards[i]['Name'],
-                    text: cards[i]['Card Text'],
-                    clan: cards[i]['Clan'],
-                    adv: cards[i]['Adv'],
-                    capacity: cards[i]['Capacity'],
-                    group: cards[i]['Group'],
-                    disciplines: cards[i]['Disciplines'],
-                    title: cards[i]['Title'],
-                    artist: cards[i]['Artist'],
-                    set: cards[i]['Set'],
-                    banned: cards[i]['Banned'],
-                    type: cards[i]['Type'],
-                    cardId: cards[i]['id'],
-                    cardType: 'crypt'});
-    }
-
-    Cards.insert({dbmetadata: 'version', version: CARD_DATABASE_VERSION});
-
-  }
 
 });
