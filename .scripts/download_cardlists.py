ab92fb3785c850543dba35c52a550f2d510eb024
Erik Gärtner <erik@gartner.io>
2020-06-16 22:10:01 +0200

    Update scripts to support new data

--- .scripts/download_cardlists.py
+++ .scripts/download_cardlists.py
@@ -8,11 +8,13 @@ import csv
 import json
 import unicodedata
 import re
+import copy
 
 import requests
+from Levenshtein import distance
 
 
-LISTING = 'http://www.lackeyccg.com/vtes/common/allsets.txt'
+LISTING = 'https://lackeyccg.com/vtes/common/sets/allsets.txt'
 
 
 def create_normalized_name(name):
@@ -42,7 +44,7 @@ def download_card_list():
         data.append({
             'name': row['Name'],
             'norm_name': create_normalized_name(row['Name']),
-            'set': row['Set'],
+            'set': row['SET'],
             'card_id': row['ImageFile'].split(',')[0],
             'expansion': row['Expansion'],
             'rarity': row['Rarity'],
@@ -51,16 +53,57 @@ def download_card_list():
             'group': row['Group'],
             'capacity': try_int(row['Capacity']),
             'disciplines': row['Discipline'].split(),
-            'pool': try_int(row['PCost']),
-            'blood': try_int(row['BCost']),
+            'pool': try_int(row['PoolCost']),
+            'blood': try_int(row['BloodCost']),
             'text': row['Text'],
             'artist': row['Artist'],
+            'new_card_id': row['ImageFile'].split(',')[0],
         })
     return data
 
 
+def upgrade_list(new_data, old_data):
+    # Convert to dictionaries
+    new_data = {d['card_id']: d for d in new_data}
+    old_data = {d['card_id']: d for d in old_data}
+
+    # Copy perfect matching
+    perfect_matches = set(new_data.keys()).intersection(old_data.keys())
+    merged_data = [new_data.pop(k) for k in perfect_matches]
+    [old_data.pop(k) for k in perfect_matches]
+
+    # Match remaining keys in old
+    for card_id, card in old_data.items():
+        closests = [(prop, distance(card_id, prop)) for prop in new_data]
+        closests.sort(key=lambda x: x[1], reverse=False)
+        new_key, best_score = closests[0]
+
+        print(f"[{best_score}] Matching {card['name']} => {new_data[new_key]['name']}", end='')
+        if best_score > 10:
+            print('. Not a good match!')
+            continue
+        else:
+            print(' Ok!')
+
+        # Add new data with old key
+        d = new_data.pop(new_key)
+        d['card_id'] = card_id
+        merged_data.append(d)
+
+    # Add remaining new
+    merged_data.extend(new_data.values())
+    print(len(merged_data))
+    return merged_data
+
+
 def main():
     data = download_card_list()
+
+    with open('../private/cards.json', 'r') as f:
+        old_data = json.load(f)
+
+    data = upgrade_list(data, old_data)
+
     with open('cards.json', 'w') as f:
         json.dump(data, f, indent=2, sort_keys=True)
 
--- .scripts/download_images.py
+++ .scripts/download_images.py
@@ -4,19 +4,35 @@ This script downloads all high-res card images from LackeyCCG.
 Uses Python 3.5.
 """
 import os
+from multiprocessing import Pool
+import json
 
 from bs4 import BeautifulSoup
 import requests
 
 
-BASE_URL = 'http://www.lackeyccg.com'
-LISTING = BASE_URL + '/vtes/high/cards/'
+LISTING = 'https://lackeyccg.com/vtes/high/cards/'
 
 
 def url2filename(url):
     if url.find('/'):
         return url.rsplit('/', 1)[1]
     return 'unknown_file_name'
+
+
+def download(args):
+    url, cards = args
+    r = requests.get(url, allow_redirects=True)
+    filename = url2filename(url)
+
+    # Use legacy card id if different for current id
+    new_card_id = filename.replace('.jpg', '')
+    filename = cards[new_card_id]['card_id'] + '.jpg'
+
+    path = 'downloads/%s' % filename
+    with open(path, 'wb') as f:
+        print('Writing %s to %s' % (url, path))
+        f.write(r.content)
 
 
 def main():
@@ -31,14 +47,14 @@ def main():
     # First link is a link to top level, not pictures
     links = links[1:]
 
+    with open('../private/cards.json', 'r') as f:
+        cards = json.load(f)
+        cards = {card['new_card_id']: card for card in cards}
+
     os.mkdir('downloads')
-    for link in links:
-        url = LISTING + link['href']
-        r = requests.get(url, allow_redirects=True)
-        filename = 'downloads/%s' % url2filename(url)
-        with open(filename, 'wb') as f:
-            print('Writing %s to %s' % (url, filename))
-            f.write(r.content)
+    urls = [(LISTING + link['href'], cards) for link in links]
+    with Pool(4) as p:
+        p.map(download, urls)
 
 
 if __name__ == '__main__':
